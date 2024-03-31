c703e9f502a42619470f5ca54e2685ba00b9d74b
Tolulope <tolulopee90@gmail.com>
2024-03-31 01:32:31 +0100

    added main.py, changed my_imp....py to transfomer.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,11 @@
 import torch
+
+text = open('data.txt', 'r')
+text = text.readlines()
+tokens = []
+for txt in text: tokens+=txt.split()
+for each in tokens: each = each.lower()
+vocabs = {word:id for id, word in enumerate(tokens)}
+token_ids = [vocabs.get(token, 0) for token in tokens]  # Handle unknown words with 0 (padding)
+# # Convert token IDs to a PyTorch tensor
+input_ids = torch.tensor(token_ids, dtype=torch.long)  # Long tensor for integer IDs
